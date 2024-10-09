d7921ed074b18cab689bbce3830feca69dbb2d97
Ziqi Zhang <ziqizhang42@gmail.com>
2024-10-09 09:21:50 -0600

    Created class FirestoreHelper to manage the Firebase Firestore reference

---
+++ app/src/main/java/com/bugoff/can_do/FirestoreHelper.java
@@ -0,0 +1,26 @@
+package com.bugoff.can_do;
+
+import com.google.firebase.firestore.FirebaseFirestore;
+
+public class FirestoreHelper {
+    private static FirestoreHelper instance;
+    private FirebaseFirestore db;
+
+    // Want Singleton pattern
+    private FirestoreHelper() {
+        db = FirebaseFirestore.getInstance();
+    }
+
+    // Get the single instance of FirestoreHelper
+    public static synchronized FirestoreHelper getInstance() {
+        if (instance == null) {
+            instance = new FirestoreHelper();
+        }
+        return instance;
+    }
+
+    // Get Firestore instance
+    public FirebaseFirestore getDb() {
+        return db;
+    }
+}
