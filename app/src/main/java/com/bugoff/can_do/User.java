7dc7bf43db894a848d1b27f8ecae263301e9d42d
Ziqi Zhang <ziqizhang42@gmail.com>
2024-10-09 16:14:54 -0600

    Added User and UserRepository classes to communicate with Firebase

---
+++ app/src/main/java/com/bugoff/can_do/User.java
@@ -0,0 +1,27 @@
+package com.bugoff.can_do;
+
+public class User {
+    private String androidId;
+    private String name;
+
+    public User(String androidId, String name) {
+        this.androidId = androidId;
+        this.name = name;
+    }
+
+    public String getAndroidId() {
+        return androidId;
+    }
+
+    public void setAndroidId(String androidId) {
+        this.androidId = androidId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
