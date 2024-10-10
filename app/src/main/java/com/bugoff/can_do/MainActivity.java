23400b211711752cec348581577bdc89385e70cc
Ziqi Zhang <ziqizhang42@gmail.com>
2024-10-09 22:04:37 -0600

    Untested: Attempted to actually interact with Firebase

--- app/src/main/java/com/bugoff/can_do/MainActivity.java
+++ app/src/main/java/com/bugoff/can_do/MainActivity.java
@@ -1,6 +1,8 @@
 package com.bugoff.can_do;
 
 import android.os.Bundle;
+import android.provider.Settings;
+import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,5 +11,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Connect to Firestore
+        UserRepository userRepository = new UserRepository();
+
+        // Get Android ID
+        String androidId = Settings.Secure.getString(getContentResolver(), Settings.Secure.ANDROID_ID);
+
+        // Get user from Firestore
+        userRepository.getUser(androidId,
+                user -> {
+                    // User found
+                    Log.d("MainActivity", "User found: " + user.getAndroidId() + " " + user.getName());
+                },
+                e -> {
+                    // User not found
+                    // Add user to Firestore
+                    User user = new User(androidId, "QWERTY XYZ");
+                    userRepository.addUser(user,
+                            aVoid -> {
+                                // User added
+                                Log.d("MainActivity", "User added: " + user.getAndroidId() + " " + user.getName());
+                            },
+                            e1 -> {
+                                // User not added
+                                Log.e("MainActivity", "Error adding user", e1);
+                            });
+                });
     }
 }
