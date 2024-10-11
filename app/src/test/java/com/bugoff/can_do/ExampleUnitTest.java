d0ecfa310c3db8043cdbdf05301cb47e5854878c
Ziqi Zhang <ziqizhang42@gmail.com>
2024-10-11 17:18:30 -0600

    Intentionally introduces a failing unit test

--- app/src/test/java/com/bugoff/can_do/ExampleUnitTest.java
+++ app/src/test/java/com/bugoff/can_do/ExampleUnitTest.java
@@ -14,4 +14,9 @@ public class ExampleUnitTest {
     public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
     }
+
+    @Test
+    public void always_fail() {
+        assertEquals(4, 3);
+    }
 }
