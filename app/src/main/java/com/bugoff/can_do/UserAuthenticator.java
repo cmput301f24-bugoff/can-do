d7678fd20bfd5fb0ac3cf326dc57c1f46ba6c9d8
Ziqi Zhang <ziqizhang42@gmail.com>
2024-10-12 15:34:01 -0600

    Added some Javadoc comments for testing

--- app/src/main/java/com/bugoff/can_do/UserAuthenticator.java
+++ app/src/main/java/com/bugoff/can_do/UserAuthenticator.java
@@ -6,7 +6,14 @@ import androidx.annotation.NonNull;
 
 // Intended to be a purely stateless class
 public class UserAuthenticator {
-    // Method to authenticate user
+    /**
+     * Method to authenticate a user using the given UserRepository.
+     * If the user exists in Firestore, it logs that the user was found.
+     * If the user does not exist, it creates a new user and adds it to Firestore.
+     *
+     * @param userRepository The repository used to access user data.
+     * @param androidId The Android ID of the user to authenticate.
+     */
     public static void authenticateUser(@NonNull UserRepository userRepository, @NonNull String androidId) {
         // Get user from Firestore
         userRepository.getUser(androidId,
@@ -14,12 +21,23 @@ public class UserAuthenticator {
                 e -> onUserNotFound(userRepository, androidId));
     }
 
-    // Handle user found
+    /**
+     * Callback method to handle when a user is found in Firestore.
+     * Logs the user's Android ID.
+     *
+     * @param user The user that was found in Firestore.
+     */
     private static void onUserFound(@NonNull User user) {
         Log.d("UserAuthenticator", "User found: " + user.getAndroidId());
     }
 
-    // Handle user not found and add the user to Firestore
+    /**
+     * Callback method to handle when a user is not found in Firestore.
+     * Creates a new user with the given Android ID and adds it to Firestore.
+     *
+     * @param userRepository The repository used to add the new user.
+     * @param androidId The Android ID of the user to add.
+     */
     private static void onUserNotFound(@NonNull UserRepository userRepository, @NonNull String androidId) {
         User newUser = new User(androidId);
         userRepository.addUser(newUser,
@@ -27,12 +45,22 @@ public class UserAuthenticator {
                 UserAuthenticator::onUserAddError);
     }
 
-    // Handle successful user addition
+    /**
+     * Callback method to handle when a user is successfully added to Firestore.
+     * Logs the user's Android ID.
+     *
+     * @param user The user that was successfully added to Firestore.
+     */
     private static void onUserAdded(@NonNull User user) {
         Log.d("UserAuthenticator", "User added: " + user.getAndroidId());
     }
 
-    // Handle error in user addition
+    /**
+     * Callback method to handle an error that occurred while adding a user to Firestore.
+     * Logs the error message.
+     *
+     * @param e The exception that occurred during the user addition process.
+     */
     private static void onUserAddError(Exception e) {
         Log.e("UserAuthenticator", "Error adding user", e);
     }
