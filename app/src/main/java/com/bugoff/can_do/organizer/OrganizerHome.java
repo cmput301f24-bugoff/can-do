7465bc5237b1ea8a2a188a3abb0ce381703b7ac8
Nick Shan <bshan1@ualberta.ca>
2024-10-29 16:47:19 -0600

    logic for changing to organizer home

---
+++ app/src/main/java/com/bugoff/can_do/organizer/OrganizerHome.java
@@ -0,0 +1,14 @@
+// Example: NewActivity.java
+package com.bugoff.can_do.organizer;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import com.bugoff.can_do.R;
+
+public class OrganizerHome extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_organizer); // Create this layout or use an existing one
+    }
+}
